500dbe2572b0521a1fe85403dd0c6504a920703f
martin <guerreiromartin@gmail.com>
2023-11-06 10:07:46 -0300

    chau

--- PBIIParcial2Futbol/src/ar/edu/unlam/pb2/ea3/CapacidadMaximaException.java
+++ PBIIParcial2Futbol/src/ar/edu/unlam/pb2/ea3/CapacidadMaximaException.java
@@ -1,10 +1,6 @@
 package ar.edu.unlam.pb2.ea3;
 
 public class CapacidadMaximaException extends Exception {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
 	public CapacidadMaximaException(String mensaje) {
 		super(mensaje);
